f65b6e117f7f3e0da05b04cd83bfce9014e1fc49
Akshay-kumar-N <67362584+Akshay-kumar-N@users.noreply.github.com>
2020-08-21 09:11:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var names=["Akshay","Joe","john","Sharan","Khaja","jitendra"];
+for(var i=0; i<names.length; i++){
+	if(names[i][0] === "J" || names[i][0] === "j"){
+		console.log("Goodbye " + names[i]);
+	}
+    else{
+    	console.log("Hello " + names[i]);
+    }
+}
